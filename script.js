6869c3708a8f28c4cfb791209db38e5b41d13d5a
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-18 15:46:04 +0100

    countdown of 30s generated, added set interval and clear interval

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ console.log('JS OK');
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei
 //  numeri da indovinare sono stati individuati.
 
+//starting countdown number
+let max = 30;
+
+//save setinterval function inside a variable to use it for clearInterval
+const countDownTimeOut = setInterval(countDownFunc, 1000);
+
 //function to generate a random number
 function generateRandomNumber(min, max) {
     const range = max - min + 1;
@@ -16,10 +22,34 @@ function generateRandomNumber(min, max) {
 }
 
 
+//function for countdown, reducing numbers and clearInterval when number is 0
+function countDownFunc() {
+    max--;
+    countDownParagraph.innerText = `${max}`;
+
+    if (max <= 0) {
+        clearInterval(countDownTimeOut);
+    }
+}
+
+
 //actions to create div, append it to body, give id
 const container = document.createElement('div');
 document.body.appendChild(container);
-container.id = 'container';
+container.id = 'containerNumbers';
+
+
+
+//actions to create div for countdown, append it to body,
+//create p and append it inside of div
+const countDownContainer = document.createElement('div');
+document.body.appendChild(countDownContainer);
+countDownContainer.id = 'containerCountDown';
+const countDownParagraph = document.createElement('p');
+countDownContainer.appendChild(countDownParagraph);
+countDownParagraph.innerText = `${max}`;
+
+
 
 //created empty array where i will store random numbers
 const arrayRandomNumbers = [];
@@ -35,6 +65,8 @@ for (let i = 0; i < 5; i++){
 }
 
 console.log(arrayRandomNumbers);
+
+
 
 
 
